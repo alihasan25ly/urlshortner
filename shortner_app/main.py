0b6f4c1b445d155a01b241800858bcf1d1eb885e
Ali Hasan <ali.hasan25@lyceumschool.edu.pk>
2025-02-04 10:24:11 +0000

    commit

--- shortner_app/main.py
+++ shortner_app/main.py
@@ -1,3 +1,3 @@
 import uvicorn
 if __name__ == "__main__":
-    uvicorn.run("index:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("index:app", host="0.0.0.0", port=3000, reload=True)
